ead715a65597a8eabfe7e4c7e1a789707123da3d
Angel <angeljcampo@gmail.com>
2021-06-14 18:21:17 -0400

    fixing some things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ skip_before_action :authenticate_user!
   def index
     
     @tweets = Tweet.order("created_at DESC")
-    @tweets = Kaminari.paginate_array(@tweets).page(params[:page]).per(4)
+    @tweets = Kaminari.paginate_array(@tweets).page(params[:page]).per(50)
     @tweet = Tweet.new
     
   end
